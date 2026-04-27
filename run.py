0274e3f781f6044f3f1d3524f7e65f5a39875930
claudioandriaan <claudio.andriaan@gmail.com>
2026-04-27 15:27:18 +0300

    fix  file_name

--- run.py
+++ run.py
@@ -2,12 +2,14 @@ import json
 from pathlib import Path
 import asyncio
 import argparse
-
+from datetime import datetime
 import seloger
 
 output = Path(__file__).parent / "data"
 output.mkdir(exist_ok=True)
-
+# update the output file name to be more descriptive like data_YY-MM-DD.json
+output_file_name = f"data_{datetime.now().strftime('%Y-%m-%d')}.json"
+output_file = output / output_file_name
 
 async def main():
     parser = argparse.ArgumentParser()
@@ -21,7 +23,7 @@ async def main():
         max_pages=args.pages
     )
 
-    with open(output / "search.json", "w", encoding="utf-8") as f:
+    with open(output_file, "w", encoding="utf-8") as f:
         json.dump(search_data, f, indent=2, ensure_ascii=False)
 
 
